f1d11d4015dfe71ab9df39f068e6da9fcb2f3e1b
Max Tsukanov <maxmarshall1995@gmail.com>
2023-10-04 17:19:30 +0300

    fixed drawer height issue & closure events

--- assets/findify-modal.js
+++ assets/findify-modal.js
@@ -1,20 +1,22 @@
+
+const toggleDrawer = (openDirection, isDrawer, container) => {
+    container.classList.toggle(openDirection);
+    if (isDrawer) document.documentElement.classList.toggle('document-overflow-mob');
+}
+
 const bindToggleEvent = (isDrawer, domRefs, classNames) => {
     const btn = document.querySelector(domRefs.toggleSection);
     const container = document.querySelector(domRefs.section);
+    const toggleEvent = () => toggleDrawer(domRefs.openDirection, isDrawer, container);
     
-    const toggle = () => {
-        container.classList.toggle(domRefs.openDirection);
-        if (isDrawer) document.documentElement.classList.toggle('document-overflow');
-    }
-    
-    btn.addEventListener('click', toggle);
+    btn.addEventListener('click', toggleEvent); 
     
     if (isDrawer) {
         const headerBtn = document.querySelector(domRefs.drawerHeader);
         const seeResultsBtn = document.querySelector(domRefs.drawerSeeResults);
 
-        headerBtn.addEventListener('click', toggle);
-        seeResultsBtn.addEventListener('click', toggle);
+        headerBtn.addEventListener('click', toggleEvent);
+        seeResultsBtn.addEventListener('click', toggleEvent);
         classNames.forEach(c => container.classList.add(c));  
     }
 }
