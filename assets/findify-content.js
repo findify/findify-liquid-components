55e0e65806903b00acb0a9f690e5b10c2cdb33ca
Warren Wolff <ww.warren.wolff@gmail.com>
2024-01-15 22:35:19 +0000

    Lazy loading and promo fixes

--- assets/findify-content.js
+++ assets/findify-content.js
@@ -1,74 +1,100 @@
+/*
+ *   Description: Bind infinite scroll event on window, and checking the div with id if shown and active and reached
+ *   @return void;
+ */
+const initFindifyContentEvents = () => {
+  const contentIntegrations = Object.keys(
+    findify.core.merchantConfig.features.content
+  ).map((id) => ({ id }));
+  contentIntegrations.forEach(({ id }) => {
+    const infiniteScrollElement = document.getElementById(
+      `${id}-infinite-scroll`
+    );
 
     /*
-*   Description: Bind infinite scroll event on window, and checking the div with id if shown and active and reached
-*   @return void;
-*/
-const initFindifyContentEvents = () => {
-    const contentIntegrations = Object.keys(findify.core.merchantConfig.features.content).map(id => ({ id }))
-    contentIntegrations.forEach(({ id }) => {
-        const infiniteScrollElement = document.getElementById(`${id}-infinite-scroll`);
+     *   @return boolean;
+     */
+    const isContentDisplayed = () => {
+      return document.getElementById(`${id}-content`).style.display === "block";
+    };
 
-        /*
-        *   @return boolean;
-        */
-        const isContentDisplayed = () => {
-            return document.getElementById(`${id}-content`).style.display === 'block';
-        }
-        /*
-        *   Description: Check if element is in viewport at the moment based on its position on screen.
-        *   @return boolean;
-        */
-        const isInViewport = (element) => {
-            if(!element) return;
-            const rect = element.getBoundingClientRect();
-            const height = (window.innerHeight || document.documentElement.clientHeight);
-            const width = (window.innerWidth || document.documentElement.clientWidth);
-            return (
-                rect.top >= 0 &&
-                rect.left >= 0 &&
-                rect.bottom <=  height + (height/1.5) && // Offset set to be 75%
-                rect.right <= width
-            );
-        }
+    const hasNextPage = (currentPage, lastPage) => currentPage <= lastPage;
 
-        const hasNextPage = (currentPage, lastPage) => currentPage <= lastPage;
+    const scrollEventOptions = { passive: true };
 
-        const scrollEventOptions = { passive: true };
-        
-        const onScrollFunction = async () => {
-            const currentPage = (findify.contents.state[id].meta.offset / findify.contents.state[id].meta.limit) + 1;
-            const lastPage = Math.ceil(findify.contents.state[id].meta.total / findify.contents.state[id].meta.limit);
-            if(isContentDisplayed() && !findify.contents.state[id].loading && isInViewport(infiniteScrollElement) && hasNextPage(currentPage, lastPage)) {
-                findify.contents.state[id] = { ...findify.contents.state[id], loading: true };
-
-                const offset = findify.contents.state[id].meta.offset + findify.contents.state[id].meta.limit;
-                const contentToLoad = { [id]: { title: findify.core.merchantConfig.features.content[id].title } };
-                const response = await findify.contents.api(contentToLoad, {
-                    offset,
-                });
-                await findify.grid.renderContents({ [id]: {
-                    ...response[id], items: [...response[id].items].splice(response[id].meta.offset, response[id].meta.offset)
-                }});
-
-                const updatedPage = (offset / findify.contents.state[id].meta.limit) + 1;
-                if(lastPage <= updatedPage) {
-                    removeLoader()
-                }
-                findify.contents.state[id].loading = false;
-            }
+    const onScrollFunction = async () => {
+      const currentPage =
+        findify.contents.state[id].meta.offset /
+          findify.contents.state[id].meta.limit +
+        1;
+      const lastPage = Math.ceil(
+        findify.contents.state[id].meta.total /
+          findify.contents.state[id].meta.limit
+      );
+      if (
+        isContentDisplayed() &&
+        !findify.contents.state[id].loading &&
+        findify.utils.isInViewport(infiniteScrollElement) &&
+        hasNextPage(currentPage, lastPage)
+      ) {
+        findify.contents.state[id] = {
+          ...findify.contents.state[id],
+          loading: true,
         };
 
-        const removeLoader = () => {
-            infiniteScrollElement?.remove();
-            window.removeEventListener('scroll', onScrollFunction, scrollEventOptions)
-        }
+        const offset =
+          findify.contents.state[id].meta.offset +
+          findify.contents.state[id].meta.limit;
+        const contentToLoad = {
+          [id]: {
+            title: findify.core.merchantConfig.features.content[id].title,
+          },
+        };
+        const response = await findify.contents.api(contentToLoad, {
+          offset,
+        });
+        await findify.grid.renderContents({
+          [id]: {
+            ...response[id],
+            items: [...response[id].items].splice(
+              response[id].meta.offset,
+              response[id].meta.offset
+            ),
+          },
+        });
 
-        const lastPage = Math.ceil(findify.contents.state[id].meta.total / findify.contents.state[id].meta.limit);
-        const currentPage = (findify.contents.state[id].meta.offset / findify.contents.state[id].meta.limit) + 1;
-        if(lastPage <= currentPage || findify.contents.state[id].meta.total === 0) {
-            removeLoader()
-        } else {
-            window.addEventListener('scroll', onScrollFunction, scrollEventOptions)
+        const updatedPage = offset / findify.contents.state[id].meta.limit + 1;
+        if (lastPage <= updatedPage) {
+          removeLoader();
         }
-    })
-}
+        findify.contents.state[id].loading = false;
+      }
+    };
+
+    const removeLoader = () => {
+      infiniteScrollElement?.remove();
+      window.removeEventListener(
+        "scroll",
+        onScrollFunction,
+        scrollEventOptions
+      );
+    };
+
+    const lastPage = Math.ceil(
+      findify.contents.state[id].meta.total /
+        findify.contents.state[id].meta.limit
+    );
+    const currentPage =
+      findify.contents.state[id].meta.offset /
+        findify.contents.state[id].meta.limit +
+      1;
+    if (
+      lastPage <= currentPage ||
+      findify.contents.state[id].meta.total === 0
+    ) {
+      removeLoader();
+    } else {
+      window.addEventListener("scroll", onScrollFunction, scrollEventOptions);
+    }
+  });
+};
---
+++ assets/findify-lazy-loading-pagination.js
@@ -0,0 +1,107 @@
+const initFindifyLazyLoadingPagination = (
+  lazy_loading_pagination_threshold
+) => {
+  let lazyLoadedCounter = 0;
+
+  const selectors = {
+    prevBtn: "findify-pagination-load-previous",
+    nextBtn: "findify-pagination-load-more",
+    loader: "findify-grid-infinite-scroll",
+    grid: "findify-product-grid",
+  };
+
+  const initialPage = findify.pagination.state.page;
+
+  const renderPage = async (page) => {
+    findify.pagination.setState({
+      ...findify.pagination.state,
+      offset: (page - 1) * findify.pagination.state.limit,
+      page: page,
+    });
+    findify.core.updateUrlParams();
+    const response = await findify.grid.api();
+    return await findify.grid.renderGridItemsHTML(
+      response.items,
+      response.promoSpots
+    );
+  };
+
+  const initPrevButton = () => {
+    const prevBtnElement = document.getElementById(selectors.prevBtn);
+    if (prevBtnElement) {
+      prevBtnElement.addEventListener("click", async () => {
+        const prevPage =
+          Math.min(initialPage, findify.pagination.state.page) - 1;
+        const items = await renderPage(prevPage);
+        const target = document.getElementById(selectors.grid);
+        items.reverse().forEach((item) => {
+          target.prepend(item);
+        });
+        if (prevPage == 1) {
+          prevBtnElement.classList.toggle("hidden");
+        }
+      });
+    }
+  };
+
+  const scrollEventOptions = { passive: true };
+  let isLoading = false;
+  const onScrollFunction = async () => {
+    const nextLoadingElement = document.getElementById(selectors.loader);
+    if (!isLoading && findify.utils.isInViewport(nextLoadingElement)) {
+      isLoading = true;
+      lazyLoadedCounter++;
+      await loadNext();
+      isLoading = false;
+      if (lazyLoadedCounter >= lazy_loading_pagination_threshold) {
+        removeLoader(true);
+      }
+    }
+  };
+
+  const removeLoader = (showLoadMoreButton) => {
+    window.removeEventListener("scroll", onScrollFunction, scrollEventOptions);
+    const nextLoadingElement = document.getElementById(selectors.loader);
+    if (nextLoadingElement) {
+      nextLoadingElement.remove();
+    }
+    if (showLoadMoreButton) {
+      const nextBtnElement = document.getElementById(selectors.nextBtn);
+      nextBtnElement.classList.toggle("hidden");
+    }
+  };
+
+  const loadNext = async () => {
+    const nextPage = Math.max(initialPage, findify.pagination.state.page) + 1;
+    const lastPage = Math.ceil(
+      findify.grid.state.meta.total / findify.pagination.state.limit
+    );
+    const items = await renderPage(nextPage);
+    const target = document.getElementById(selectors.grid);
+    items.forEach((item) => {
+      target.append(item);
+    });
+    if (nextPage == lastPage) {
+      const nextBtnElement = document.getElementById(selectors.nextBtn);
+      nextBtnElement.classList.toggle("hidden");
+      removeLoader();
+    }
+  };
+
+  const initNextEvents = () => {
+    const nextLoadingElement = document.getElementById(selectors.loader);
+    if (nextLoadingElement) {
+      window.addEventListener("scroll", onScrollFunction, scrollEventOptions);
+    }
+    const nextBtnElement = document.getElementById(selectors.nextBtn);
+    if (nextBtnElement) {
+      nextBtnElement.addEventListener("click", () => loadNext());
+    }
+  };
+
+  const init = () => {
+    initPrevButton();
+    initNextEvents();
+  };
+  init();
+};
